72af2c7616c32bfd39a996e774cb4c5bc6be1e08
Quentin Mathé <quentin.mathe@gmail.com>
2013-04-22 14:43:41 +0000

    Added SCKClangSourceFile.h to the umbrella header
    git-svn-id: http://svn.gna.org/svn/etoile/trunk/Etoile/Languages/SourceCodeKit@8243 7b3f36aa-e6db-49c1-8b4c-66eac1997e64

--- SourceCodeKit.h
+++ SourceCodeKit.h
@@ -1,6 +1,7 @@
 #import "SCKSourceCollection.h"
 #import "SCKCodeCompletionResult.h"
 #import "SCKSourceFile.h"
+#import "SCKClangSourceFile.h"
 #import "SCKSyntaxHighlighter.h"
 #import "SCKTextTypes.h"
 #import "SCKIntrospection.h"
